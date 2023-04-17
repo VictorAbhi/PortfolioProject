fb1b7420406e3aed584e1759ad064a4a2b00db77
Abhishek Adhikari <98272076+VictorAbhi@users.noreply.github.com>
2023-04-17 11:09:28 +0545

    Update and rename SQLQuery2.sql to QueriesForTableau.sql

--- QueriesForTableau.sql
+++ QueriesForTableau.sql
@@ -4,7 +4,6 @@ Queries used for Tableau Project
 
 
 
--- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
@@ -13,9 +12,6 @@ where continent is not null
 --Group By date
 order by 1,2
 
-
-
--- 2. 
 
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
@@ -27,16 +23,12 @@ Group by location
 order by TotalDeathCount desc
 
 
--- 3.
-
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
-
--- 4.
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
@@ -46,7 +38,6 @@ Group by Location, Population, date
 order by PercentPopulationInfected desc
 
 
--- 1.
 
 Select dea.continent, dea.location, dea.date, dea.population
 , MAX(vac.total_vaccinations) as RollingPeopleVaccinated
@@ -62,7 +53,6 @@ order by 1,2,3
 
 
 
--- 2.
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
@@ -81,8 +71,6 @@ order by 1,2
 --order by 1,2
 
 
--- 3.
-
 
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
@@ -95,7 +83,6 @@ order by TotalDeathCount desc
 
 
 
--- 4.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
@@ -117,7 +104,6 @@ order by PercentPopulationInfected desc
 
 
 
--- 5.
 
 --Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 --From PortfolioProject..CovidDeaths
@@ -133,7 +119,7 @@ where continent is not null
 order by 1,2
 
 
--- 6. 
+
 
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
@@ -154,7 +140,6 @@ Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
 From PopvsVac
 
 
--- 7. 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
